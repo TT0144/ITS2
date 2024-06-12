2e37933b7bd19280fb497d6f2d7f6c14a8f42a15
MSI\2230169 <2230169@ecc.ac.jp>
2024-06-12 21:29:32 +0900

    ランキングのJSとPHP

--- 山本/php/API.php
+++ 山本/php/API.php
@@ -7,8 +7,9 @@ $diaryarrow = array();
 $data = file_get_contents('php://input');
 $json_deco=json_decode($data,true);
 $branch = $json_deco["branch"];
+$sort_text = $json_deco["sort_text"];
 // $branch = 1;
-
+// $sort_text = "a";
 
 $dsn = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=" . DB_CHARSET;
 try {
@@ -18,7 +19,7 @@ try {
     die("Connection failed: " . $e->getMessage());
 }
 try {
-  //
+  
   if($branch == 0){
   // スポット情報を取得
   $stmtSpots = $conn->prepare("
@@ -39,17 +40,28 @@ try {
     'SPOT'=>spotdatainarray($popularSpots),
     'DIARY'=>diarydatainarray($popularDiaries)
   );
-  
-  }elseif($branch == 1){  
-    $stmtSpots = $conn->prepare("SELECT SPOT_ID,SPOTNAME, PHOTO, REMARKS FROM SPOTS_POSTING");
-    $stmtSpots->execute();
-    $popularSpots = $stmtSpots->fetchAll(PDO::FETCH_ASSOC);
-  
+  }elseif($branch == 1){
+    $sql = "SELECT SPOT_ID,SPOTNAME,PHOTO,REMARKS FROM SPOTS_POSTING";
+    $where = " where 1=1";
+
+    //s
+    if(!empty($sort_text)){
+      $sort_textLike = "%" . $sort_text . "%";
+      $where .=" AND spotname LIKE :sort_text OR remarks LIKE :sort_text";
+    }
+
+    $stmt = $conn->prepare($sql.$where);
+    
+    if(!empty($sort_text)){
+      $stmt->bindParam(':sort_text',$sort_textLike, PDO::PARAM_STR);
+    }
+    $stmt->execute();
+    $popularSpots = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    // echo($popularSpots);
     $returndata[]=array(
       'SPOT'=>spotdatainarray($popularSpots)
     );
-  
-  }else{
+  }elseif($branch == 2){
 
   // 人気日記を取得
     $stmtDiaries = $conn->prepare("SELECT TITLE, PHOTO,DIARY_ID,TEXT FROM DIARYS_POSTING ");
@@ -60,6 +72,27 @@ try {
     'DIARY'=>diarydatainarray($popularDiaries)
   );
 
+  }else{
+  //こっから↓スポットのみの文字検索（簡易版）
+  $sql = "SELECT SPOT_ID,SPOTNAME,PHOTO,REMARKS FROM SPOTS_POSTING";
+  $where = " where 1=1";
+
+  if(!empty($sort_text)){
+    $sort_textLike = "%" . $sort_text . "%";
+    $where .=" AND spotname LIKE :sort_text OR remarks LIKE :sort_text";
+  }
+
+  $stmt = $conn->prepare($sql.$where);
+  
+  if(!empty($sort_text)){
+    $stmt->bindParam(':sort_text',$sort_textLike, PDO::PARAM_STR);
+  }
+  $stmt->execute();
+  $popularSpots = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  // echo($popularSpots);
+  $returndata[]=array(
+    'SPOT'=>spotdatainarray($popularSpots)
+  );
   }
 } catch (PDOException $e) {
   die("Connection failed: " . $e->getMessage());
